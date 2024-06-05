bb96eeefe1c746c44c8541b1ce935b3d54ddb5c9
RafaelsCarneiro <144498890+RafaelsCarneiro@users.noreply.github.com>
2024-06-05 16:20:29 -0300

    Add files via upload

---
+++ src/app/livro-dados/livro-dados.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
+import { Livro } from '../livro';
+import { Router } from '@angular/router'; // Importando Router
+
+@Component({
+  selector: 'app-livro-dados',
+  templateUrl: './livro-dados.component.html',
+  styleUrls: ['./livro-dados.component.css']
+})
+export class LivroDadosComponent implements OnInit {
+  public livro: Livro = new Livro();
+  public autoresForm: string = '';
+  public editoras = [];
+
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService,
+    private router: Router // Injetando Router
+  ) {}
+
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+  }
+
+  incluir = (): void => {
+    this.livro.autores = this.autoresForm.split('\n');
+    this.servLivros.incluir(this.livro);
+    this.router.navigateByUrl('/lista');
+  }
+}
